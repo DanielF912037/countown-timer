f4aa07f5ee271e38ec9aab41430261cc8eaf709f
DanielF912037 <snsksnbspeter@gmail.com>
2023-10-13 20:10:56 +0100

    Update countdown_timer.py
    used tkinter to make a application window

--- countdown_timer.py
+++ countdown_timer.py
@@ -1,36 +1,58 @@
-# import the time module
 import time
-
-# define the countdown function
+import tkinter as tk
+from tkinter import font
+# Define the countdown function
 def countdown(t):
-
-    while t:
+    while t > 0:
         mins, secs = divmod(t, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
+        timer = '{:02d}:{:02d}'.format(int(mins), int(secs))
 
-        # Print the timer with carriage return to overwrite the previous timer
-        print(timer, end="\r")
-
-        # Wait for 1 second 
-        time.sleep(1)   
+        # Update the label with the current timer value
+        label.config(text=timer)
+        root.update()
+        time.sleep(1)
         t -= 1
 
     # Display a message when the countdown reaches zero
-    print("Your time is up")  
+    label.config(text="Your time is up")
 
-# Get the input from the user for the countdown time
-t = int(input("Enter the time you want in seconds: "))
+# Function to start the countdown
+def start_countdown():
+    global t
+    try:
+        t = float(entry.get())
+        countdown(t)
+    except ValueError:
+        label.config(text="Invalid input. Please enter a number.")
 
-while True:
-    countdown(t)
+# Function to reset the timer
+def reset_timer():
+    global t
+    label.config(text="")
+    # Clear the input field
+    entry.delete(0, tk.END) 
 
-    # asks the yser if they want to reset the timer
-    reset = input("Do you wantr to reset the timer? (y/n):  ").strip().lower()
-    
-    # cheak if the user presses y or yes
-    if reset in ['yes', 'y']: 
-        t = int(input("Enter the time you want for the new countdown in seconds: "))
-    else:
-        break
+# Create a Tkinter window
+root = tk.Tk()
+root.title("Countdown Timer")
 
-print("Timer stoped.")
+# Set the window size
+root.geometry("400x200")
+
+# Create an entry widget for user input
+entry = tk.Entry(root, width=20, font=font.Font(size=16), justify='center')
+entry.pack()
+
+# Create a button to start the countdown
+start_button = tk.Button(root, text="Start Countdown", command=start_countdown, font=font.Font(size=14), height=1)
+start_button.pack()
+
+# Create a label to display the countdown timer
+label = tk.Label(root, text="", font=font.Font(size=28))
+label.pack()
+
+# Create a button to reset the timer
+reset_button = tk.Button(root, text="Reset Timer", command=reset_timer, font=font.Font(size=14), height=1)
+reset_button.pack()
+
+root.mainloop()
